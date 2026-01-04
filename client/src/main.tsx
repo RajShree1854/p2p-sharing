f05f8efdd1b1e5bfa5a228cb3a7edecd7661882c
RajShree1854 <207335599+RajShree1854@users.noreply.github.com>
2026-01-04 16:57:48 +0530

    refactor: move WSProvider to P2PApp component for better context management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { WSProvider } from "./context/WebSocketContext";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <WSProvider>
-    <App />
-  </WSProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
