59e93cc8d7903be9eca1cb652b38875eab012694
RajShree1854 <207335599+RajShree1854@users.noreply.github.com>
2026-01-11 02:50:49 +0530

    fix: correct import package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import P2PApp from "./pages/P2PApp";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
